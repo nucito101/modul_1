de1a284690757e921957a578ba56544072b74d7d
nucito101 <andrenucitoschmidt@gmail.com>
2025-09-09 14:03:41 +0200

    Update ProjectSection.tsx

--- Tag_45_React_Router_V2/aufgaben/projekt-architect-portfolio/src/components/projectSection/ProjectSection.tsx
+++ Tag_45_React_Router_V2/aufgaben/projekt-architect-portfolio/src/components/projectSection/ProjectSection.tsx
@@ -6,7 +6,7 @@ import Btn from "../btn/Btn"
 export default function ProjectSection() {
   return (
     <section className="mx-auto max-w-6xl px-6 py-14">
-      <SectionContainer text="Uur Orjects" />
+      <SectionContainer text="Our Projects" />
 
       <div className="mt-6 grid gap-6 grid-cols-[1fr_1fr]">
         <ProjectCard src="/projects_01.png" title="Sample Project" />
